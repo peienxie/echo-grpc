c56c077a01c40b7ab364dee2169a17168029b016
Peien Xie <peienxieit@gmail.com>
2021-05-16 16:23:43 +0800

    add client and server side main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"echo-grpc/echo"
+	"echo-grpc/pb"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+var  (
+	host = "localhost"
+	port = 9001
+)
+
+func main() {
+	apiListener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", host, port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("Listening on %s\n", apiListener.Addr().String())
+
+	echoServer := echo.NewEchoServer()
+	
+	grpc := grpc.NewServer()
+	pb.RegisterEchoServiceServer(grpc, echoServer)
+	grpc.Serve(apiListener)
+}
